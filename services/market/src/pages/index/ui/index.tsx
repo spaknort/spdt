a55d231f3c7a6c7b0b79529707ed3da7dcb3ffec
spaknort <145426237+spaknort@users.noreply.github.com>
2024-01-07 21:02:38 +0800

    add shared package

--- services/market/src/pages/index/ui/index.tsx
+++ services/market/src/pages/index/ui/index.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
+import { sum } from '@packages/shared'
 
 export const Index: FC = () => {
     return (
         <div className="index">
             Index
+            { sum(5, 5) }
         </div>
     )
 }
